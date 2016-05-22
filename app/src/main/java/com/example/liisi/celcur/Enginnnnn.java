110a82714b1d9b0f74caf51c5f8f42e0334d6d6a
Klaups <kristian.laupa.59@gmail.com>
2016-05-22 11:33:21 +0300

    Newest

---
+++ app/src/main/java/com/example/liisi/celcur/Enginnnnn.java
@@ -0,0 +1,152 @@
+package com.example.liisi.celcur;
+
+import android.widget.Button;
+
+import java.text.DecimalFormat;
+
+public class Enginnnnn {
+
+    private String[] numbers = {"", ""};
+    private char operator = '\u0000';
+    private double savedValue = 0;
+    private boolean flag = false;// true if there is first number
+    private static final int maxLength = 14;
+
+
+    public void addNumber(String number) {
+        if (!flag && numbers[0].length() <= maxLength) {
+            numbers[0] += number;
+        } else if (flag && numbers[1].length() <= maxLength) {
+            numbers[1] += number;
+        }
+    }
+
+    public void setOperator(String op) {
+        if (numbers[0] != "") {
+            operator = op.charAt(0);
+            flag = true;
+        }
+    }
+
+    public void calculateAns() {
+
+        double a = Double.parseDouble(numbers[0]);
+        double b = Double.parseDouble(numbers[1]);
+
+        if (!numbers[0].equals("") && !numbers[1].equals("")) {
+            switch (operator) {
+                case '+':
+                    savedValue = a + b;
+                    break;
+                case '-':
+                    savedValue = a - b;
+                    break;
+                case '*':
+                    savedValue = a * b;
+                    break;
+                case '/':
+                    savedValue = a / b;
+                    break;
+            }
+            if(savedValue == Double.POSITIVE_INFINITY){
+                return;
+            } else {
+                savedValue = round(savedValue);
+            }
+            numbers[0] = String.valueOf(savedValue);
+            numbers[1] = "";
+        }
+    }
+
+
+    public boolean operatorCheck(Button btn) {
+        if (btn.getText().equals("/") || btn.getText().equals("*") || btn.getText().equals("+") ||
+                btn.getText().equals("-")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public double round(double a) {
+        return (double) Math.round(a * 100.0) / 100.0;
+    }
+
+
+    public void reset() {
+        savedValue = 0;
+        operator = '\u0000';
+        numbers = new String[]{"", ""};
+        flag = false;
+    }
+
+    public boolean checkIfTwoNumbers() {
+        if ((!numbers[0].equals("") && !numbers[1].equals("")) && (!numbers[0].equals(".") && !numbers[1].equals("."))) {
+            return true;
+        }
+        return false;
+    }
+
+    public String getFirstNr() {
+        return numbers[0];
+    }
+
+    public String getSecondNr() {
+        return numbers[1];
+    }
+
+    public boolean getFlag() {
+        return flag;
+    }
+
+    public String returnSavedValue() {
+        if(savedValue == Double.POSITIVE_INFINITY ){
+            reset();
+            return "Math Error";
+        }else {
+            return String.valueOf(savedValue);
+        }
+
+    }
+
+    public String[] returnNumbers() {
+        return numbers;
+    }
+
+    public char returnChar() {
+        return operator;
+    }
+
+    public boolean returnFlag() {
+        return flag;
+    }
+
+    public void setSavedValue(String value) {
+        savedValue = Double.parseDouble(value);
+    }
+
+    public void setNumbers(String[] nr) {
+        numbers = nr;
+    }
+
+    public void setOp(char ch) {
+        operator = ch;
+    }
+
+    public void setFlag(boolean bo) {
+        flag = bo;
+    }
+
+
+    //DEBUG
+    public char forDebug() {
+        return operator;
+    }
+
+    public String forDebug1() {
+        return numbers[0] + " " + numbers[1];
+    }
+
+
+}
+
---
+++ app/src/main/java/com/example/liisi/celcur/MainActivity.java
@@ -0,0 +1,237 @@
+package com.example.liisi.celcur;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Display;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String STATE_NUMBERS = "Numbers in memory";
+    private static final String STATE_FLAG = "Boolean";
+    private static final String STATE_SAVEDVALUE = "LastAnswer";
+    private static final String STATE_OPERATOR = "Operator";
+    private static final String STATE_CURRENTNUMBER = "CurrentNr";
+
+    private static final String TAG = "MainActivity";
+    private static String strDouble = "";
+    private TextView textViewShow;
+    private int orient;
+
+    private Enginnnnn c = new Enginnnnn();
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        orient = getScreenOrientation();
+        super.onCreate(savedInstanceState);
+        if (BuildConfig.DEBUG) {
+            Log.d(TAG, "onCreateCalled");
+        }
+        setContentView(R.layout.activity_main);
+        textViewShow = (TextView) findViewById(R.id.textViewNumber);
+        textViewShow.setText(strDouble);
+
+
+        if (savedInstanceState != null) {
+            if (BuildConfig.DEBUG) {
+                Log.d(TAG, "Restoring state");
+            }
+
+            c.setNumbers(savedInstanceState.getStringArray(STATE_NUMBERS));
+            c.setOp(savedInstanceState.getChar(STATE_OPERATOR));
+            c.setFlag(savedInstanceState.getBoolean(STATE_FLAG));
+            c.setSavedValue(savedInstanceState.getString(STATE_SAVEDVALUE));
+            strDouble = savedInstanceState.getString(STATE_CURRENTNUMBER);
+            showContent();
+        }
+
+    }
+
+    public void btnClicked(View view) {
+        Button btn = (Button) view;
+        String idAsString = btn.getResources().getResourceName(btn.getId());
+
+        //DEBUG
+        if (BuildConfig.DEBUG) {
+            Log.d(TAG, "Button pressed :" + idAsString);
+        }
+
+        if (btn.getText().equals("C")) {
+            resetAnswer();
+        } else if (btn.getText().equals("=")) {
+            calculateAnswer();
+        } else if (!c.operatorCheck(btn)) {
+            saveNumber(btn);
+        } else if (c.operatorCheck(btn)) {
+            setOperator(btn);
+        }
+
+
+        //DEBUG
+        String orientation = "";
+        if (orient == 1) {
+            orientation = "portrait";
+        } else if (orient == 2) {
+            orientation = "landscape";
+        } else {
+            orientation = "ERROR";
+        }
+        if (BuildConfig.DEBUG) {
+            Log.d(TAG, String.valueOf(c.forDebug()));
+            Log.d(TAG, c.forDebug1());
+            Log.d(TAG, c.returnSavedValue());
+            Log.d(TAG, orientation);
+        }
+
+    }
+
+    public void showContent() {
+        setTextSize();
+        textViewShow.setText(strDouble);
+    }
+
+
+    public void setOperator(Button button) {
+        strDouble = "";
+        if (c.checkIfTwoNumbers()) {
+            calculateAnswer();
+        }
+        c.setOperator(button.getText().toString());
+    }
+
+
+    public void calculateAnswer() {
+        if (c.checkIfTwoNumbers()) {
+            c.calculateAns();
+            strDouble = c.returnSavedValue();
+            showContent();
+        }
+
+    }
+
+    public void resetAnswer() {
+        strDouble = "";
+        c.reset();
+        showContent();
+    }
+
+    public void saveNumber(Button btn) {
+
+        String nr = btn.getText().toString();
+        checkIfInfinity();
+        //check if number contains coma
+        if (btn.getText().toString().contains(".") && strDouble.contains(".")) {
+            return;
+            //first number not set flag = false
+        } else if (!c.getFlag()) {
+            strDouble = strDouble + btn.getText();
+            c.addNumber(nr);
+            showContent();
+        } else {
+            c.addNumber(nr);
+            strDouble = c.getSecondNr();
+            showContent();
+        }
+    }
+
+    public void setTextSize() {
+        if (strDouble.length() > 10 && orient == 1) {
+            textViewShow.setTextSize(40);
+        } else if (strDouble.length() > 10 && orient == 2) {
+            textViewShow.setTextSize(60);
+        }
+    }
+
+    public void checkIfInfinity(){
+        if(textViewShow.getText().toString().equals("Math Error")) {
+            resetAnswer();
+        }else {
+
+        }
+    }
+
+
+    // SOURCE http://stackoverflow.com/questions/14955728/getting-orientation-of-android-device
+    public int getScreenOrientation() {
+
+        // Query what the orientation currently really is.
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            return 1; // Portrait Mode
+
+        } else if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            return 2;   // Landscape mode
+        }
+        return 0;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+
+        savedInstanceState.putString(STATE_SAVEDVALUE, c.returnSavedValue());
+        savedInstanceState.putChar(STATE_OPERATOR, c.returnChar());
+        savedInstanceState.putBoolean(STATE_FLAG, c.returnFlag());
+        savedInstanceState.putStringArray(STATE_NUMBERS, c.returnNumbers());
+
+        if (BuildConfig.DEBUG) {
+            Log.d(TAG, "Saving strDouble to STATE, value is: " + c.getFirstNr());
+        }
+        savedInstanceState.putString(STATE_CURRENTNUMBER, c.getFirstNr());
+
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (BuildConfig.DEBUG) {
+            Log.d(TAG, "onStartCalled");
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (BuildConfig.DEBUG) {
+            Log.d(TAG, "onResumeCalled");
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (BuildConfig.DEBUG) {
+            Log.d(TAG, "onPauseCalled");
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (BuildConfig.DEBUG) {
+            Log.d(TAG, "onStopCalled");
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (BuildConfig.DEBUG) {
+            Log.d(TAG, "onDestroyCalled");
+        }
+    }
+
+
+}
